361d969f3b46ec620639b928801af15c30f3b4f5
KaranSaini2219 <155142275+KaranSaini2219@users.noreply.github.com>
2025-02-06 08:27:06 +0000

    data_types

---
+++ .devcontainer/basics/data_types.js
@@ -0,0 +1,16 @@
+"use strict";//treat js code as newer version
+//alert(3+3) //not work becoz we are using nodejs not browser
+let name="karan"
+let age=20
+let isloggedin=false
+let state;
+/*number=> 2 to power 53
+big int
+string=>""
+boolean=>true/false
+null=>standalone value
+undefined
+
+*/
+console.log(typeof("karan"));
+console.log(typeof null);// though null is a standalone value but it is a object
