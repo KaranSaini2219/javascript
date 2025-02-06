33c3ecdd685cd13c9d76b2e7e740de0ef1ff4aba
KaranSaini2219 <155142275+KaranSaini2219@users.noreply.github.com>
2025-02-06 09:38:11 +0000

    conversion_2.js

---
+++ .devcontainer/basics/conversion_2.js
@@ -0,0 +1,14 @@
+let str1="hello"
+let str2=" karan"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(1+"2"+3)
+console.log(1+2+2+"2")
+console.log(+ true)
+console.log(-true)
+console.log(+"")
+
