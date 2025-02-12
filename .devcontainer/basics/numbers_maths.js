36783a4dab9f70b2ec304373f8cf1f9b3fcfad37
KaranSaini2219 <155142275+KaranSaini2219@users.noreply.github.com>
2025-02-12 14:00:10 +0000

    maths_and_numbers.js

---
+++ .devcontainer/basics/numbers_maths.js
@@ -0,0 +1,29 @@
+//const score=400
+//const balance=new Number(100)
+//console.log(balance)
+//console.log(score)
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+//const othernumber=3.14
+//console.log(othernumber.toPrecision(2))
+//
+//const hundreds=100000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//**************maths******************* */
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.6))
+//console.log(Math.ceil(4.5))
+//console.log(Math.floor(4.7))
+//console.log(Math.max(1,2,3,4))
+//console.log(Math.min(1,2,3,4))
+//
+
+//formula for generating random values between some min and max value
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
