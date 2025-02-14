db9ef8a3dab66bdb97cc45e26b04b9709473b8b0
KaranSaini2219 <155142275+KaranSaini2219@users.noreply.github.com>
2025-02-14 20:06:32 +0000

    date

---
+++ .devcontainer/basics/date.js
@@ -0,0 +1,11 @@
+let mydate=new Date()
+console.log(mydate)
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toLocaleString())
+
+let mycreateddate=new Date(2023,0,23)//in this format months start from 0
+console.log(mycreateddate)
+let mycreateddate2=new Date("2023,1,23")//in this format months start from 1
+console.log(mycreateddate2)
+
